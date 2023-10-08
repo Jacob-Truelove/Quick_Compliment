ef4629cd203f61ae499b29c6f2acb69a7faf163b
Jacob-Truelove <jet4853@mavs.uta.edu>
2023-10-07 22:47:44 -0500

    added profile class

---
+++ headers/Factors.hpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+
+enum category{
+    LOOKS, TALENT, PERSONALITY
+};
+
+struct qualities{
+    std::string look;
+    std::string talent;
+    std::string personality;
+};
+
+struct compliment{
+    std::string comp;
+    category cat;
+};
